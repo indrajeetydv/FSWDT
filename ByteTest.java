9dd747d3a2e674830460a9e3854c1ddb7355d5fe
“indrajeetydv” <dev.indrajeetydv@gmail.com>
2021-01-13 00:28:51 +0530

    Data types and literals

---
+++ ByteTest.java
@@ -0,0 +1,20 @@
+public class ByteTest{
+	public static void main(java.lang.String[] args){
+		//-128 to 127
+		byte b=10;
+
+		System.out.println(b);
+		//
+		float f=10;
+		System.out.println(f);
+
+
+		//
+
+		byte b1=(byte)132;
+
+		System.out.println(b1);
+
+
+	}
+}
