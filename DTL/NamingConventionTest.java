7cb91c7aa8f6e537e5e685614321fd618449143b
“indrajeetydv” <dev.indrajeetydv@gmail.com>
2021-01-13 01:29:15 +0530

    Naming convention

---
+++ DTL/NamingConventionTest.java
@@ -0,0 +1,13 @@
+public class NamingConventionTest{
+	public static void main(String[] args){
+		String emp="Rajat";
+		String Emp="Rajat";
+
+		String $emp="indrajeet";
+		String _emp="indrajeet";
+
+		String 1emp="indrajeet";
+		System.out.println(emp+"\t"+Emp+"\t"+$emp+"\t"+_emp);
+
+	}
+}
